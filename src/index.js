fcf2deaf7fe45cc8e723ba08ee22fafb223d83fd
sumansjs <suman.hansada@gmail.com>
2021-03-28 15:58:55 +0530

    Added webpack and prettify formatter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function Hi() {
-  return <h1>Hi.</h1>;
+	return <h1>Hi.</h1>;
 }
 
 render(<Hi />, document.getElementById('app'));
