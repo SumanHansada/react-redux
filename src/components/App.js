2977199d792442bab1ff46a24036658c96ab1219
sumansjs <suman.hansada@gmail.com>
2021-04-04 20:20:39 +0530

    Added ManageCoursePage with routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
 import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
 import HomePage from './home/HomePage';
 import PageNotFound from './PageNotFound';
 
 function App() {
-	return (
-		<div className="container-fluid">
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage}></Route>
-				<Route path="/about" component={AboutPage}></Route>
-				<Route path="/courses" component={CoursesPage}></Route>
-				<Route component={PageNotFound}></Route>
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+        <Route path="/courses" component={CoursesPage}></Route>
+        <Route path="/course/:slug" component={ManageCoursePage}></Route>
+        <Route path="/course" component={ManageCoursePage}></Route>
+        <Route component={PageNotFound}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
