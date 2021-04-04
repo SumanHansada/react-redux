3c0db27574edd0c224b5d7827e8a96872641884d
sumansjs <suman.hansada@gmail.com>
2021-04-04 16:52:44 +0530

    Added thunk middleware in configurestore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from './reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
 	// Added for Redux Dev Tools
@@ -11,6 +12,6 @@ export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
 		initialState,
-		composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+		composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
 	);
 }
