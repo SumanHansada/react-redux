f66af5f96de96c15e2732b663f8096bda7da125e
sumansjs <suman.hansada@gmail.com>
2021-04-04 17:11:22 +0530

    Displayed courses on CoursePage.js

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,33 +5,38 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
+  componentDidMount() {
+    this.props.actions.loadCourses().catch((error) => {
+      alert('Loading courses failed' + error);
+    });
+  }
 
-	render() {
-		return (
-			<>
-				<h2>Courses</h2>
-				{this.props.courses.map((course) => (
-					<div key={course.title}>{course.title}</div>
-				))}
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-	courses: PropTypes.array.isRequired,
-	actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
-	return {
-		courses: state.courses,
-	};
+  return {
+    courses: state.courses,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(courseActions, dispatch),
-	};
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
