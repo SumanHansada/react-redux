f93e92d11af79b9259ea8d84f23695210d46827a
sumansjs <suman.hansada@gmail.com>
2021-04-07 21:36:11 +0530

    Added Save Course Thunk and Action Creators

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,23 +1,38 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
 export function loadCourses() {
-  return function (dispatch) {
-    return courseApi
-      .getCourses()
-      .then((courses) => {
-        dispatch(loadCoursesSuccess(courses));
-      })
-      .catch((error) => {
-        throw error;
-      });
+  return async function (dispatch) {
+    try {
+      const courses = await courseApi.getCourses();
+      dispatch(loadCoursesSuccess(courses));
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+export function saveCourse(course) {
+  return async function (dispatch, getState) {
+    try {
+      const savedCourse = await courseApi.saveCourse(course);
+      course.id
+        ? dispatch(updateCourseSuccess(savedCourse))
+        : dispatch(createCourseSuccess(savedCourse));
+    } catch (error) {
+      throw error;
+    }
   };
 }
