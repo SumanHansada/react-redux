be493c0feb930ed332ef4fbeb61a11fbd9410faf
sumansjs <suman.hansada@gmail.com>
2021-04-08 20:22:07 +0530

    Populated ManageCoursePage by mapStateToProps

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -19,16 +19,19 @@ function ManageCoursePage({
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    if (courses.length === 0)
+    if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert('Loading courses failed' + error);
       });
+    } else {
+      setCourse({ ...props.course });
+    }
 
     if (authors.length === 0)
       loadAuthors().catch((error) => {
         alert('Loading courses failed' + error);
       });
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -66,9 +69,18 @@ ManageCoursePage.propTypes = {
   history: PropTypes.object.isRequired,
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug == slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   };
