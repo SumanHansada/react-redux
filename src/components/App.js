151e7080a953812f5c31800c18ff0851f1cb2d4e
sumansjs <suman.hansada@gmail.com>
2021-05-02 22:44:55 +0530

    Added Lazy Loading and Code Splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import AboutPage from './about/AboutPage';
-import Header from './common/Header';
-import CoursesPage from './courses/CoursesPage';
-import ManageCoursePage from './courses/ManageCoursePage'; // eslint-disable-line import/no-named-as-default
-import HomePage from './home/HomePage';
-import PageNotFound from './PageNotFound';
 import { ToastContainer } from 'react-toastify';
+import Spinner from './common/Spinner';
+import Header from './common/Header';
 import 'react-toastify/dist/ReactToastify.css';
+const ManageCoursePage = lazy(() => import('./courses/ManageCoursePage')); // eslint-disable-line import/no-named-as-default
+const HomePage = lazy(() => import('./home/HomePage'));
+const CoursesPage = lazy(() => import('./courses/CoursesPage'));
+const AboutPage = lazy(() => import('./about/AboutPage'));
+const PageNotFound = lazy(() => import('./PageNotFound'));
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/about" component={AboutPage}></Route>
-        <Route path="/courses" component={CoursesPage}></Route>
-        <Route path="/course/:slug" component={ManageCoursePage}></Route>
-        <Route path="/course" component={ManageCoursePage}></Route>
-        <Route component={PageNotFound}></Route>
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route path="/courses" component={CoursesPage}></Route>
+          <Route path="/course/:slug" component={ManageCoursePage}></Route>
+          <Route path="/course" component={ManageCoursePage}></Route>
+          <Route component={PageNotFound}></Route>
+        </Switch>
+      </Suspense>
       <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
